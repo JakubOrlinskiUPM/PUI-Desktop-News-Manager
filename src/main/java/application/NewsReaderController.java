3b3d5f32d2c6b2d004762ace35ceb1300f784bea
jakuborlinski <j.orlinski@student.utwente.nl>
2021-10-21 17:19:03 +0200

    Start implementing the home page (layout, basic callbacks)

--- src/main/java/application/NewsReaderController.java
+++ src/main/java/application/NewsReaderController.java
@@ -1,44 +1,19 @@
 /**
- * 
+ *
  */
 package application;
-
-import java.io.File;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.util.function.Predicate;
 
 
 import application.news.Article;
 import application.news.Categories;
 import application.news.User;
-import application.utils.JsonArticle;
-import application.utils.exceptions.ErrorMalFormedArticle;
-import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.transformation.FilteredList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Label;
-import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.web.WebView;
-import javafx.stage.FileChooser;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import javafx.stage.Window;
-import javafx.stage.FileChooser.ExtensionFilter;
 import serverConection.ConnectionManager;
 
 /**
@@ -47,49 +22,104 @@ import serverConection.ConnectionManager;
  */
 public class NewsReaderController {
 
-	private NewsReaderModel newsReaderModel = new NewsReaderModel();
-	private User usr;
+    private NewsReaderModel newsReaderModel = new NewsReaderModel();
+    private User usr;
 
-	//TODO add attributes and methods as needed
+    @FXML
+    private ListView<Article> articleListView;
+    private FilteredList<Article> filteredData;
 
-	public NewsReaderController() {
-		//TODO
-		//Uncomment next sentence to use data from server instead dummy data
-		//newsReaderModel.setDummyData(false);
-		//Get text Label
-		
-	}
+    @FXML
+    private ChoiceBox<Categories> categoriesChoiceBox;
 
-		
+    @FXML
+    private Label articleContent;
 
-	private void getData() {
-		//TODO retrieve data and update UI
-		//The method newsReaderModel.retrieveData() can be used to retrieve data  
-	}
+    @FXML
+    private ImageView articleImageView;
 
-	/**
-	 * @return the usr
-	 */
-	User getUsr() {
-		return usr;
-	}
 
-	void setConnectionManager (ConnectionManager connection){
-		this.newsReaderModel.setDummyData(false); //System is connected so dummy data are not needed
-		this.newsReaderModel.setConnectionManager(connection);
-		this.getData();
-	}
-	
-	/**
-	 * @param usr the usr to set
-	 */
-	void setUsr(User usr) {
-		
-		this.usr = usr;
-		//Reload articles
-		this.getData();
-		//TODO Update UI
-	}
+
+    //TODO add attributes and methods as needed
+
+    public NewsReaderController() {
+        //TODO
+        //Uncomment next sentence to use data from server instead dummy data
+        //newsReaderModel.setDummyData(false);
+        //Get text Label
+
+    }
+
+    @FXML
+    void initialize() {
+        categoriesChoiceBox.setValue(Categories.ALL);
+
+        categoriesChoiceBox.setOnAction(ev -> {
+            Categories selectedCategory = this.categoriesChoiceBox.getSelectionModel().getSelectedItem();
+            filteredData = new FilteredList<>(newsReaderModel.getArticles(), article -> (
+                    selectedCategory == null || selectedCategory == Categories.ALL ||
+                            article.getCategory() == selectedCategory.toString()));
+
+            this.articleListView.setItems(filteredData);
+        });
+
+        this.articleListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Article>() {
+            @Override
+            public void changed(ObservableValue<? extends Article> observable, Article oldValue, Article newValue) {
+                if (newValue != null) {
+                    articleImageView.setImage(newValue.getImageData());
+                    articleContent.setText(newValue.getAbstractText());
+                } else { //Nothing selected
+                    articleImageView.setImage(null);
+                    articleContent.setText("");
+                }
+            }
+
+        });
+    }
+
+
+    private void getData() {
+        Image img3 = new Image("file:./src/main/resources/application/images/abstract1.jpg");
+
+        System.out.println(img3.isError());
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+
+
+        //TODO retrieve data and update UI
+        //The method newsReaderModel.retrieveData() can be used to retrieve data
+        newsReaderModel.retrieveData();
+
+        filteredData = new FilteredList<>(newsReaderModel.getArticles(), article -> true);
+
+        this.articleListView.setItems(filteredData);
+
+        this.categoriesChoiceBox.setItems(newsReaderModel.getCategories());
+    }
+
+    /**
+     * @return the usr
+     */
+    User getUsr() {
+        return usr;
+    }
+
+    void setConnectionManager(ConnectionManager connection) {
+//		this.newsReaderModel.setDummyData(false); //System is connected so dummy data are not needed
+        this.newsReaderModel.setConnectionManager(connection);
+        this.getData();
+    }
+
+    /**
+     * @param usr the usr to set
+     */
+    void setUsr(User usr) {
+
+        this.usr = usr;
+        //Reload articles
+        this.getData();
+        //TODO Update UI
+    }
 
 
 }

5c864b3726397e3db3ab9e6a025ddb7f68ae7e29
jakuborlinski <j.orlinski@student.utwente.nl>
2021-10-21 18:17:07 +0200

    Implement the rest of the main view

--- src/main/java/application/NewsReaderController.java
+++ src/main/java/application/NewsReaderController.java
@@ -14,6 +14,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.web.HTMLEditor;
+import javafx.scene.web.WebView;
 import serverConection.ConnectionManager;
 
 /**
@@ -33,10 +35,13 @@ public class NewsReaderController {
     private ChoiceBox<Categories> categoriesChoiceBox;
 
     @FXML
-    private Label articleContent;
+    private WebView articleContent;
 
     @FXML
     private ImageView articleImageView;
+
+    @FXML
+    private Button readMore;
 
 
 
@@ -61,38 +66,44 @@ public class NewsReaderController {
                             article.getCategory() == selectedCategory.toString()));
 
             this.articleListView.setItems(filteredData);
+            this.articleListView.getSelectionModel().select(0);
         });
 
         this.articleListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Article>() {
             @Override
             public void changed(ObservableValue<? extends Article> observable, Article oldValue, Article newValue) {
-                if (newValue != null) {
-                    articleImageView.setImage(newValue.getImageData());
-                    articleContent.setText(newValue.getAbstractText());
-                } else { //Nothing selected
-                    articleImageView.setImage(null);
-                    articleContent.setText("");
-                }
+                articleSelected(oldValue, newValue);
             }
-
         });
+    }
+
+    private void articleSelected(Article oldValue, Article newValue) {
+        if (newValue != null) {
+            articleImageView.setImage(newValue.getImageData());
+            articleContent.getEngine().loadContent(newValue.getAbstractText());
+
+            readMore.setOnAction(ev -> {
+                this.routeToArticle(newValue);
+            });
+        } else { //Nothing selected
+            articleImageView.setImage(null);
+            articleContent.getEngine().loadContent("");
+            readMore.setOnAction(null);
+        }
+    }
+
+    private void routeToArticle(Article art) {
+        System.out.println("Routing to " + art.getTitle());
     }
 
 
     private void getData() {
-        Image img3 = new Image("file:./src/main/resources/application/images/abstract1.jpg");
-
-        System.out.println(img3.isError());
-        System.out.println("Working Directory = " + System.getProperty("user.dir"));
-
-
-        //TODO retrieve data and update UI
-        //The method newsReaderModel.retrieveData() can be used to retrieve data
         newsReaderModel.retrieveData();
 
         filteredData = new FilteredList<>(newsReaderModel.getArticles(), article -> true);
-
         this.articleListView.setItems(filteredData);
+
+        this.articleListView.getSelectionModel().select(0);
 
         this.categoriesChoiceBox.setItems(newsReaderModel.getCategories());
     }
@@ -120,6 +131,5 @@ public class NewsReaderController {
         this.getData();
         //TODO Update UI
     }
-
 
 }
